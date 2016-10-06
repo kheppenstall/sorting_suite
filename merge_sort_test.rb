6b8e8721027dabd07d298cb7dbcf916036dea406
Kyle Heppenstall <kyle.heppenstall@gmail.com>
2016-10-06 13:02:17 -0600

    Second commit

--- merge_sort_test.rb
+++ merge_sort_test.rb
@@ -5,7 +5,7 @@ sorter = MergeSort.new
 # p sorter.sort([])
 # p sorter.sort([1])
 # p sorter.sort([1,2])
-# p sorter.sort([2,1])
+p sorter.sort([2,1])
 p sorter.sort(["d", "b", "a", "c"])
 # p sorter.sort([4,3,2,1,1,0])
 # p sorter.sort([4,3,1,2])
